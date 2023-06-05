4eed4516cc073e4a43dda89c75f74e77a1c9c4df
Nino <anthony@delcey.fr>
2023-06-05 20:11:21 +0200

    Demonstrate how to UnitTest GMS Task and cancelled coroutines

--- data/src/test/java/com/despaircorp/data/user/UserRepositoryFirestoreUnitTest.kt
+++ data/src/test/java/com/despaircorp/data/user/UserRepositoryFirestoreUnitTest.kt
@@ -1,16 +1,18 @@
 package com.despaircorp.data.user
 
+import assertk.assertThat
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import com.despaircorp.data.utils.TestCoroutineRule
 import com.despaircorp.domain.user.model.UserEntity
+import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
 import io.mockk.every
-import io.mockk.just
 import io.mockk.mockk
-import io.mockk.runs
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.CancellationException
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -22,29 +24,106 @@ class UserRepositoryFirestoreUnitTest {
         private const val DEFAULT_EMAIL = "DEFAULT_EMAIL"
         private const val DEFAULT_PHOTO_URL = "DEFAULT_PHOTO_URL"
     }
-    
+
     @get:Rule
     val testCoroutineRule = TestCoroutineRule()
-    
+
     private val firestore: FirebaseFirestore = mockk()
-    
+
     private val userRepositoryFirestore = UserRepositoryFirestore(
         firestore,
     )
-    
+
     @Before
     fun setup() {
+        coEvery {
+            firestore
+                .collection("users")
+                .document(DEFAULT_ID)
+                .set(provideUserDto())
+        } returns getDefaultSetUserTask()
+    }
 
-    }
-    
     @Test
-    fun `nominal case - get user`() = testCoroutineRule.runTest {
-        val result = userRepositoryFirestore.getUser(provideUserEntity().id)
-        println(result)
-        
+    fun `nominal case - save user`() = testCoroutineRule.runTest {
+        // When
+        val result = userRepositoryFirestore.saveUser(provideUserEntity())
+
+        // Then
+        assertThat(result).isTrue()
+        coVerify(exactly = 1) {
+            firestore
+                .collection("users")
+                .document(DEFAULT_ID)
+                .set(provideUserDto())
+        }
+        confirmVerified(firestore)
     }
-    
-    
+
+    @Test
+    fun `error case - save user's coroutine is cancelled during run`() = testCoroutineRule.runTest {
+        // Given
+        coEvery {
+            firestore
+                .collection("users")
+                .document(DEFAULT_ID)
+                .set(provideUserDto())
+        } throws CancellationException()
+
+        // When
+        val result = userRepositoryFirestore.saveUser(provideUserEntity())
+
+        // Then
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `error case - save user launches an exception`() = testCoroutineRule.runTest {
+        // Given
+        val exception = Exception()
+        coEvery {
+            firestore
+                .collection("users")
+                .document(DEFAULT_ID)
+                .set(provideUserDto())
+        } throws exception
+
+        // When
+        val result = userRepositoryFirestore.saveUser(provideUserEntity())
+
+        // Then
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `error case - save user task launches an exception`() = testCoroutineRule.runTest {
+        // Given
+        val exception = Exception()
+        coEvery {
+            firestore
+                .collection("users")
+                .document(DEFAULT_ID)
+                .set(provideUserDto())
+        } returns getDefaultSetUserTask {
+            every { this@getDefaultSetUserTask.exception } returns exception
+        }
+
+        // When
+        val result = userRepositoryFirestore.saveUser(provideUserEntity())
+
+        // Then
+        assertThat(result).isFalse()
+    }
+
+    private inline fun getDefaultSetUserTask(crossinline mockkBlock: Task<Void>.() -> Unit = {}): Task<Void> = mockk {
+        every { isComplete } returns true
+        every { exception } returns null
+        every { isCanceled } returns false
+        every { result } returns mockk()
+
+        mockkBlock(this)
+    }
+
     //Region out
     private fun provideUserEntity() = UserEntity(
         id = DEFAULT_ID,
@@ -53,7 +132,7 @@ class UserRepositoryFirestoreUnitTest {
         photoUrl = DEFAULT_PHOTO_URL,
         isEating = false
     )
-    
+
     private fun provideUserDto() = UserDto(
         uuid = DEFAULT_ID,
         name = DEFAULT_NAME,
