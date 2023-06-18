bf1367fbbbc4152f483476ad5f00464f852ac0bb
monokouma <haaxiz@gmail.com>
2023-06-19 00:08:19 +0200

    fuck it

--- data/src/test/java/com/despaircorp/data/user/UserRepositoryFirestoreUnitTest.kt
+++ data/src/test/java/com/despaircorp/data/user/UserRepositoryFirestoreUnitTest.kt
@@ -1,5 +1,6 @@
 package com.despaircorp.data.user
 
+import app.cash.turbine.test
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
@@ -7,8 +8,13 @@ import assertk.assertions.isTrue
 import com.despaircorp.data.utils.TestCoroutineRule
 import com.despaircorp.domain.user.model.UserEntity
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.auth.User
 import com.google.firebase.firestore.ktx.toObject
 import io.mockk.coEvery
@@ -17,13 +23,20 @@ import io.mockk.confirmVerified
 import io.mockk.core.ValueClassSupport.boxedValue
 import io.mockk.every
 import io.mockk.just
+import io.mockk.justRun
 import io.mockk.mockk
 import io.mockk.runs
+import io.mockk.slot
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runCurrent
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import kotlin.time.Duration.Companion.seconds
 
 class UserRepositoryFirestoreUnitTest {
     companion object {
@@ -51,13 +64,7 @@ class UserRepositoryFirestoreUnitTest {
                 .set(provideUserDto())
         } returns getDefaultSetUserTask()
         
-        coEvery {
-            firestore.collection("users")
-                .document(DEFAULT_ID)
-                .get()
-                .result
-                .toObject<UserDto>()
-        } returns provideUserDto()
+        
     }
 
     @Test
@@ -131,20 +138,38 @@ class UserRepositoryFirestoreUnitTest {
         assertThat(result).isFalse()
     }
 
-    /*
+    
     @Test
     fun `nominal case - get user`() = testCoroutineRule.runTest {
-        val result = userRepositoryFirestore.getUser(DEFAULT_ID)
-       
-        assertThat(result).isEqualTo(provideUserEntity())
-        coVerify(exactly = 1) {
-            firestore.collection("users")
-                .document(DEFAULT_ID)
-                .get()
+        // Mock Firestore dependencies
+        val mockSnapshot = mockk<DocumentSnapshot>()
+        val mockDocumentRef = mockk<DocumentReference>()
+        val mockCollectionRef = mockk<CollectionReference>()
+        val mockFirestore = mockk<FirebaseFirestore>()
+        val mockRegistration = mockk<ListenerRegistration>()
+    
+        // Mock behavior
+        every { mockSnapshot.toObject<UserDto>() } returns UserDto("123", "Alice", "alice@example.com", "picture_url")
+        every { mockDocumentRef.addSnapshotListener(any()) } answers {
+            val listener = firstArg<(DocumentSnapshot?, Exception?) -> Unit>()
+            listener(mockSnapshot, null)
+            mockRegistration
         }
-        confirmVerified(firestore)
+        every { mockCollectionRef.document("123") } returns mockDocumentRef
+        every { mockFirestore.collection("users") } returns mockCollectionRef
+    
+        // Test the function
+        userRepositoryFirestore.getUser("123").test(timeout = 2.seconds) {
+            val userEntity = awaitError()
+           println(userEntity.localizedMessage)
+            awaitComplete()
+        }
+    
+        // Verify that the document method was called with the expected argument
+        coVerify { mockCollectionRef.document("123") }
+    
     }
-     */
+    
     private inline fun getDefaultSetUserTask(crossinline mockkBlock: Task<Void>.() -> Unit = {}): Task<Void> = mockk {
         every { isComplete } returns true
         every { exception } returns null
