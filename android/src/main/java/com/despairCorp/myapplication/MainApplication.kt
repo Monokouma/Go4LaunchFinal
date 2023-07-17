2cf24f45525dca6ef47e5e233480b8f5726f8b3a
monokouma <haaxiz@gmail.com>
2023-07-17 15:13:23 +0200

    repairing google service firebaseApp not initialized error

--- android/src/main/java/com/despairCorp/myapplication/MainApplication.kt
+++ android/src/main/java/com/despairCorp/myapplication/MainApplication.kt
@@ -1,7 +1,14 @@
 package com.despaircorp.myapplication
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
--- ui/src/main/java/com/despaircorp/ui/login/LoginActivity.kt
+++ ui/src/main/java/com/despaircorp/ui/login/LoginActivity.kt
@@ -1,11 +1,10 @@
 package com.despaircorp.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.despaircorp.ui.bottom_navigation.BottomNavigationActivity
 import com.despaircorp.ui.databinding.LoginActivityBinding
 import com.despaircorp.ui.utils.viewBinding
@@ -34,23 +33,25 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         FacebookSdk.sdkInitialize(this)
+        
         FirebaseApp.initializeApp(this)
         callbackManager = CallbackManager.Factory.create()
-    
+        
         binding.loginActFacebookButton.setOnClickListener {
-            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email", "public_profile"))
+            LoginManager.getInstance()
+                .logInWithReadPermissions(this, listOf("email", "public_profile"))
         }
-    
+        
         binding.loginActGoogleButton.setOnClickListener {
             val signInIntent = googleSignInClient.signInIntent
             startActivityForResult(signInIntent, GOOGLE_SIGN_IN)
         }
-    
+        
         binding.loginActGithubButton.setOnClickListener {
             val githubProvider = OAuthProvider.newBuilder("github.com")
             githubProvider.addCustomParameter("login", "your-email@gmail.com")
             githubProvider.scopes = listOf("user:email")
-        
+            
             val pendingResultTask = FirebaseAuth.getInstance().pendingAuthResult
             if (pendingResultTask != null) {
                 pendingResultTask.addOnSuccessListener {
@@ -68,40 +69,41 @@ class LoginActivity : AppCompatActivity() {
                     }
             }
         }
-    
+        
         LoginManager.getInstance().registerCallback(
             callbackManager,
             object : FacebookCallback<LoginResult> {
                 override fun onSuccess(result: LoginResult) {
                     handleFacebookAccessToken(result.accessToken)
                 }
-            
+                
                 override fun onCancel() {
                     Toast.makeText(this@LoginActivity, "Login Cancel", Toast.LENGTH_LONG).show()
                 }
-            
+                
                 override fun onError(error: FacebookException) {
                     Toast.makeText(this@LoginActivity, error.message, Toast.LENGTH_LONG).show()
                 }
             })
-    
+        
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("57559716164-d8pa0c4f9jr0ju11odg5amhaiphjsjp7.apps.googleusercontent.com")
             .requestEmail()
             .build()
-    
+        
         googleSignInClient = GoogleSignIn.getClient(this, gso)
-    
+        
         viewModel.loginViewActionLiveData.observe(this) {
             when (it.getContentIfNotHandled()) {
                 LoginAction.GoToMainActivity -> startActivity(BottomNavigationActivity.navigate(this))
                 LoginAction.ErrorMessage -> {
                     Toast.makeText(this, "Error login", Toast.LENGTH_SHORT).show()
                 }
+                
                 null -> Unit
             }
         }
-    
+        
         if (FirebaseAuth.getInstance().currentUser != null) {
             startActivity(BottomNavigationActivity.navigate(this))
         }
@@ -127,28 +129,30 @@ class LoginActivity : AppCompatActivity() {
     private fun handleFacebookAccessToken(token: AccessToken) {
         
         val credential = FacebookAuthProvider.getCredential(token.token)
-        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                viewModel.onUserConnected()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Authentication failed.",
-                    Toast.LENGTH_SHORT
-                ).show()
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    viewModel.onUserConnected()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
-        }
     }
     
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                viewModel.onUserConnected()
-            } else {
-                task.exception?.printStackTrace()
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    viewModel.onUserConnected()
+                } else {
+                    task.exception?.printStackTrace()
+                }
             }
-        }
     }
     
     companion object {
